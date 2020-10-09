79381eee6875c1479f03617efa9a7050a0e6056b
mdivband <mohammad.divband@gmail.com>
2020-10-09 20:16:57 +0100

    first version

---
+++ init.py
@@ -0,0 +1,143 @@
+import time
+import os
+import arcade
+import objects
+import argparse
+
+# Simply collects the belief error and the confidence of the swarm at each 5 steps
+# Could be used with different swarm sizes, reliability ranges and percentages, and communication noise
+def init(SWARM_SIZE = 15, ARENA_WIDTH = 600, ARENA_HEIGHT = 600, name_of_experiment = time.time(), INPUT_TIME = 300, GRID_X = 40, GRID_Y = 40,
+               disaster_size = 1, disaster_location = 'random', operator_size = 1, operator_location = 'random', reliability = (100, 101), unreliability_percentage = 0, 
+               moving_disaster = False, communication_noise = 0, alpha = 10, normal_command = None, command_period = 0, constant_repulsion = False, 
+               operator_vision_radius = 150, communication_range = 8, vision_range = 2, velocity_weight_coef = 0.01, boundary_repulsion = 1, aging_factor = 0.9999,
+               gp = False, gp_step = 50, maze = None, through_walls = True,
+               communication_noise_strength = 0, communication_noise_prob = 0, positioning_noise_strength = 0, positioning_noise_prob = 0, sensing_noise_strength = 0, sensing_noise_prob = 0):
+    
+    sim = objects.SwarmSimulator(ARENA_WIDTH, ARENA_HEIGHT, name_of_experiment, SWARM_SIZE, INPUT_TIME, GRID_X, GRID_Y)
+    
+    sim.setup(disaster_size, disaster_location, operator_size, operator_location, reliability[0], reliability[1], unreliability_percentage, moving_disaster, communication_noise, 
+              alpha, normal_command, command_period, constant_repulsion, operator_vision_radius,
+              communication_range, vision_range, velocity_weight_coef, boundary_repulsion, aging_factor, gp, gp_step, maze, through_walls,
+              communication_noise_strength, communication_noise_prob, positioning_noise_strength, positioning_noise_prob,sensing_noise_strength, sensing_noise_prob)  
+
+    if (not os.path.isdir(name_of_experiment)):
+        os.mkdir(name_of_experiment)
+    if (not os.path.isdir(name_of_experiment + '\\data')):
+        os.mkdir(name_of_experiment + '\\data')
+         
+    sim.directory = str(name_of_experiment + '\\data\\results'+ str(time.time()))
+    
+    while os.path.isdir(sim.directory):
+        sim.directory = str(name_of_experiment + '\\data\\results' + str(time.time()))
+
+    directory = sim.directory
+        
+    os.mkdir(directory)
+    sim.log_setup(directory)      
+    arcade.run()                 
+    
+    #sim.plot_heatmaps(sim.random_drone_confidence_maps, 'Random drone confidence')
+    #sim.plot_heatmaps(sim.random_drone_belief_maps, 'Random drone belief')
+    
+    #sim.plot_boxplots(sim.swarm_confidence, 'Swarm confidence over time')
+    #sim.plot_boxplots(sim.swarm_internal_error, 'Swarm belief map error over time')
+    
+    #sim.plot_heatmaps(sim.operator_confidence_maps, 'Operator confidence')
+    #sim.plot_heatmaps(sim.operator_belief_maps, 'Operator belief')
+    
+    #sim.plot_boxplots(sim.operator_confidence, 'Operator confidence over time')
+    #sim.plot_boxplots(sim.operator_internal_error, 'Operator belief map error over time')
+    
+
+    sim.save_positions(sim, directory)
+    sim.save_boxplots(sim.swarm_confidence, 'confidence_time', directory)
+    sim.save_boxplots(sim.swarm_internal_error, 'belief_error', directory)
+
+    sim.save_boxplots(sim.operator_confidence, 'operator_confidence_time', directory)
+    sim.save_boxplots(sim.operator_internal_error, 'operator_belief_error', directory)
+    
+    print('END')
+
+def merge(list1, list2):       
+    merged_list = [] 
+    for i in range(max((len(list1), len(list2)))):   
+        while True: 
+            try: 
+                tup = (list1[i], list2[i]) 
+            except IndexError: 
+                if len(list1) > len(list2): 
+                    list2.append('') 
+                    tup = (list1[i], list2[i]) 
+                elif len(list1) < len(list2): 
+                    list1.append('') 
+                    tup = (list1[i], list2[i]) 
+                continue  
+            merged_list.append(tup) 
+            break
+    return merged_list 
+
+if __name__ == '__main__':    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('-size', type = int, default = 15) #swarm_size
+    parser.add_argument('-name', type = str, default = str(time.time())) #experiment_name
+    parser.add_argument('-d_size', type=int, default = 1)
+    parser.add_argument('-d_xs', nargs='+', type=int, default = [500])
+    parser.add_argument('-d_ys', nargs='+', type=int, default = [500])
+    parser.add_argument('-d_move', type = bool, default = False)#moving disaster
+    parser.add_argument('-op_size', type=int, default = 1)
+    parser.add_argument('-op_xs', nargs='+', type=int, default = [450])
+    parser.add_argument('-op_ys', nargs='+', type=int, default = [300])
+    parser.add_argument('-noise', type = int, default = 0) #communication_noise
+    parser.add_argument('-r_min', type = int, default = 100) #min_reliability
+    parser.add_argument('-r_max', type = int, default = 100) #max_reliability
+    parser.add_argument('-r_perc', type = int, default = 0) #unreliability_percentage
+    parser.add_argument('-cmd', type = str, default = None) #normal_command
+    parser.add_argument('-cmd_t', type = int, default = 0) #command_period
+    parser.add_argument('-const_repel', type = bool, default = False) #constant_repulsion
+    parser.add_argument('-alpha', type = float, default = 10) #command strength
+    parser.add_argument('-comm_range', type = int, default = 4) #communication_range
+    parser.add_argument('-vis_range', type = int, default = 2) #vision_range
+    parser.add_argument('-w', type = float, default = 0.01) #velocity_weight_coef
+    parser.add_argument('-bound', type = float, default = 1) #boundary_repulsion
+    parser.add_argument('-aging', type = float, default = 0.9999) #boundary_repulsion
+    parser.add_argument('-hum_r', type = int, default = 100)#operator_vision_radius    
+    parser.add_argument('-height', type = int, default = 600) #arena_height
+    parser.add_argument('-width', type = int, default = 600) #arena_width
+    parser.add_argument('-grid_x', type = int, default = 40) #grid_x
+    parser.add_argument('-grid_y', type = int, default = 40) #grid_y
+    parser.add_argument('-input_time', type = int, default = 300) #input_time
+    parser.add_argument('-gp', type = bool, default = False) #gaussian processes
+    parser.add_argument('-gp_step', type = int, default = 50) #gaussian processes step
+    parser.add_argument('-maze', type = str, default = None) #maze
+    parser.add_argument('-walls', type = bool, default = False) #communication through walls
+
+
+    # New experiments
+    parser.add_argument('-communication_noise_strength', type = float, default = 0) 
+    parser.add_argument('-communication_noise_prob', type = float, default = 0) 
+
+    parser.add_argument('-positioning_noise_strength', type = float, default = 0) 
+    parser.add_argument('-positioning_noise_prob', type = float, default = 0) 
+    
+    parser.add_argument('-sensing_noise_strength', type = float, default = 0) 
+    parser.add_argument('-sensing_noise_prob', type = float, default = 0) 
+    
+    args = parser.parse_args()
+    
+    disasters_locations = merge(args.d_xs, args.d_ys)
+    operators_locations = merge(args.op_xs, args.op_ys)
+    
+    if args.d_size > len(args.d_xs):
+        disasters_locations += [('random', 'random')]*(args.d_size - len(args.d_xs))
+        
+    if args.op_size > len(args.op_xs):
+        operators_locations += [('random', 'random')]*(args.op_size - len(args.op_xs))
+
+    experiment(args.size, args.width, args.height, args.name, args.input_time, args.grid_x, args.grid_y, len(disasters_locations), disasters_locations, 
+                   len(operators_locations), operators_locations, (args.r_min, args.r_max), args.r_perc, args.noise, args.d_move, args.alpha, args.cmd, 
+                   args.cmd_t, args.const_repel, args.hum_r, args.comm_range, args.vis_range, args.w, args.bound, args.aging, args.gp, args.gp_step,
+                   args.maze, args.walls,
+                   args.communication_noise_strength, args.communication_noise_prob,
+                   args.positioning_noise_strength, args.positioning_noise_prob,
+                   args.sensing_noise_strength, args.sensing_noise_prob)
---
+++ run.py
@@ -0,0 +1,226 @@
+import sys
+import subprocess
+import re
+
+''' COMMANDS:
+-size            type = int     default = 15           swarm_size
+-name            type = str     default = time.time()  experiment_name
+-d_size          type = int     default = 1            number of disasters
+-d_xs            type = int     default = [500]        disaster_x's
+-d_ys            type = int     default = [500]        disaster_y's
+-d_move          type = bool    default = False        moving_disaster
+-op_size         type = int     default = 1            operator_size
+-op_x            type = int     default = [450]        operator_x's
+-op_y            type = int     default = [300]        operator_y's
+-noise           type = int     default = 0            communication_noise
+-r_min           type = int     default = 100          min_reliability
+-r_max           type = int     default = 100          max_reliability
+-r_perc          type = int     default = 0            unreliability_percentage
+-cmd             type = str     default = None         normal_command
+-cmd_t           type = int     default = 0            command_period
+-const_repel     type = bool    default = False        constant_repulsion
+-alpha           type = int     default = 10           command strength
+-comm_range      type = int     default = 8            communication_range
+-vis_range       type = int     default = 2            vision_range
+-w               type = int     default = 0.01         velocity_weight_coef
+-bound           type = int     default = 1            boundary_repulsion
+-aging           type = int     default = 0.9999       boundary_repulsion
+-hum_r           type = int     default = 150          operator_vision_radius
+-height          type = int     default = 600          arena_height
+-width           type = int     default = 600          arena_width
+-grid_x          type = int     default = 40           grid_x
+-grid_y          type = int     default = 40           grid_y
+-input_time      type = int     default = 300          input_time
+-gp              type = bool    default = False        gaussian processes
+-gp_step         type = int     default = 50           gaussian processes_step
+-maze            type = str     default = True         maze type
+'''
+  
+def compare_swarm_sizes(swarm_sizes = [1, 5, 10, 15, 20, 25, 50]):    
+    cmds = []   
+    for size in swarm_sizes:             
+        cmds.append('-name "first experiment_{0}" -d_x 500 -d_y 500'.format(size) + ' -size ' + size)
+        cmds.append('-name "first experiment_{0}" -d_x 100 -d_y 500'.format(size) + ' -size ' + size)
+        cmds.append('-name "first experiment_{0}" -d_x 300 -d_y 100'.format(size) + ' -size ' + size)
+    return cmds    
+
+def simple_experiment():    
+    cmds = []                
+    cmds.append('-name "second experiment" -d_x 500 -d_y 500')
+    cmds.append('-name "second experiment" -d_x 100 -d_y 500')
+    cmds.append('-name "second experiment" -d_x 300 -d_y 100')
+    return cmds
+    
+def reliability_experiment(min_reliability = 95, max_reliability = 100, unreliability_percentage = [0, 20, 50]):    
+    cmds = []    
+    addition = ' -r_min '+ str(min_reliability) + ' -r_max '+ str(max_reliability)     
+    for level in unreliability_percentage:     
+        cmds.append('-name "third experiment_{0}" -d_x 500 -d_y 500'.format(level) + addition + ' -r_perc ' + str(level) )
+        cmds.append('-name "third experiment_{0}" -d_x 100 -d_y 500'.format(level) + addition + ' -r_perc ' + str(level) )
+        cmds.append('-name "third experiment_{0}" -d_x 300 -d_y 100'.format(level) + addition + ' -r_perc ' + str(level) )
+    return cmds
+    
+def noise_experiment(noise_levels = [0, 10, 15]):    
+    cmds = []        
+    for level in noise_levels:        
+        cmds.append('-name "fourth experiment_{0}" -d_x 500 -d_y 500 -noise '.format(level) + str(level))
+        cmds.append('-name "fourth experiment_{0}" -d_x 100 -d_y 500 -noise '.format(level) + str(level))
+        cmds.append('-name "fourth experiment_{0}" -d_x 300 -d_y 100 -noise '.format(level) + str(level))
+    return cmds
+ 
+def ordinary_repulsion(ranges = [50, 100, 150], t = 25):    
+    cmds = []        
+    addition = ' -cmd "center repel" -cmd_t ' + str(t) 
+    for r in ranges:        
+        cmds.append('-name "ordinary repulsion experiment_r{0}_t{1}" -d_x 500 -d_y 500'.format(r, t) + addition + ' -hum_r ' + str(r))
+        cmds.append('-name "ordinary repulsion experiment_r{0}_t{1}" -d_x 100 -d_y 500'.format(r, t) + addition + ' -hum_r ' + str(r))
+        cmds.append('-name "ordinary repulsion experiment_r{0}_t{1}" -d_x 300 -d_y 100'.format(r, t) + addition + ' -hum_r ' + str(r))
+    return cmds
+        
+def ordinary_attraction(ranges = [50, 100, 150], t = 25):    
+    cmds = []        
+    addition = ' -cmd "center attract" -cmd_t ' + str(t) 
+    for r in ranges:        
+        cmds.append('-name "ordinary attraction experiment_r{0}_t{1}" -d_x 500 -d_y 500'.format(r, t) + addition + ' -hum_r ' + str(r))
+        cmds.append('-name "ordinary attraction experiment_r{0}_t{1}" -d_x 100 -d_y 500'.format(r, t) + addition + ' -hum_r ' + str(r))
+        cmds.append('-name "ordinary attraction experiment_r{0}_t{1}" -d_x 300 -d_y 100'.format(r, t) + addition + ' -hum_r ' + str(r))
+    return cmds
+
+def constant_center_repel(ranges = [50, 100, 150]):    
+    cmds = []         
+    for r in ranges:        
+        cmds.append('-name "constant repulsion experiment_r{0}" -d_x 500 -d_y 500 -const_repel "True"'.format(r) + ' -hum_r ' + str(r))
+        cmds.append('-name "constant repulsion experiment_r{0}" -d_x 100 -d_y 500 -const_repel "True"'.format(r) + ' -hum_r ' + str(r))
+        cmds.append('-name "constant repulsion experiment_r{0}" -d_x 300 -d_y 100 -const_repel "True"'.format(r) + ' -hum_r ' + str(r))
+    return cmds
+
+def moving_disaster():    
+    cmds = []             
+    cmds.append('-name "moving_disaster" -d_x 500 -d_y 500 -d_move "True"')
+    cmds.append('-name "moving_disaster" -d_x 100 -d_y 500 -d_move "True"')
+    cmds.append('-name "moving_disaster" -d_x 500 -d_y 100 -d_move "True"')
+    return cmds
+
+def obstacle():
+    return ['-name "obstacle_gp" -d_x 200 -d_y 400 -gp "True" -size 2 -grid_x 60 -grid_y 60']
+
+def maze(maze_type, additions = '-op_xs 500 -op_ys 300 -d_xs 100 -d_ys 300', through_walls = False):
+    return ['-name "maze4_{0}_{1}" -maze "{0}" -walls {1} '.format(maze_type, through_walls) + additions]
+
+def new_maze(maze_type):
+    return maze(maze_type, '-op_xs 500 -op_ys 300 -d_xs 100 -d_ys 300 ')
+
+
+def maze_experiments(maze_type = None, operator_range = [150, 100, 50], drone_range = [8, 4], gp = False, addition = '-op_xs 500 -op_ys 300 -d_xs 100 -d_ys 300'):
+
+    cmds = []         
+    for op_r in operator_range:
+        for r in drone_range:
+            if gp == True:
+                if maze_type == None:
+                    cmds.append('-name "gp_no_maze_op_{0}_dr_{1}" -gp "True" -hum_r {0} -comm_range {1} '.format(op_r,r) + addition)
+                else:                
+                    cmds.append('-name "gp_maze_{0}_op_{1}_dr_{2}" -maze "{0}" -gp "True" -aging 0.995 -w 0.005 -hum_r {1} -comm_range {2} '.format(maze_type,op_r,r) + addition)
+            else:
+                if maze_type == None:
+                    cmds.append('-name "gp_no_maze_op_{0}_dr_{1}" -hum_r {0} -comm_range {1} '.format(op_r,r) + addition)
+                else:  
+                    cmds.append('-name "gp_maze_{0}_op_{1}_dr_{2}" -maze "{0}" -aging 0.995 -w 0.005 -hum_r {1} -comm_range {2} '.format(maze_type,op_r,r) + addition)
+
+    return cmds
+
+def communication_noise(communication_noise_prob = [0.1, 0.25, 0.5, 0.75, 1]):
+    cmds = []
+    for p in communication_noise_prob:
+        cmds.append('-name "communication_noise experiment_{0}" -d_x 500 -d_y 500 -communication_noise_prob {0}'.format(p))
+        cmds.append('-name "communication_noise experiment_{0}" -d_x 100 -d_y 500 -communication_noise_prob {0}'.format(p))
+        cmds.append('-name "communication_noise experiment_{0}" -d_x 300 -d_y 100 -communication_noise_prob {0}'.format(p))
+    return cmds
+
+def positioning_noise(positioning_noise_strength = [1, 2, 3], positioning_noise_prob = [0.1, 0.25, 0.5, 0.75, 1]):
+    cmds = []
+    for s in positioning_noise_strength:
+        for p in positioning_noise_prob:
+            cmds.append('-name "positioning_noise experiment_{0}_{1}" -d_x 500 -d_y 500 -positioning_noise_strength {0} -positioning_noise_prob {1}'.format(s,p))
+            cmds.append('-name "positioning_noise experiment_{0}_{1}" -d_x 100 -d_y 500 -positioning_noise_strength {0} -positioning_noise_prob {1}'.format(s,p))
+            cmds.append('-name "positioning_noise experiment_{0}_{1}" -d_x 300 -d_y 100 -positioning_noise_strength {0} -positioning_noise_prob {1}'.format(s,p))
+    return cmds
+
+def sensing_noise(sensing_noise_strength = [0.1, 0.25, 0.5, 0.75, 1], sensing_noise_prob = [0.1, 0.25, 0.5, 0.75, 1]):
+    cmds = []
+    for s in sensing_noise_strength:
+        for p in sensing_noise_prob:
+            cmds.append('-name "sensing_noise experiment_{0}_{1}" -d_x 500 -d_y 500 -sensing_noise_strength {0} -sensing_noise_prob {1}'.format(s,p))
+            cmds.append('-name "sensing_noise experiment_{0}_{1}" -d_x 100 -d_y 500 -sensing_noise_strength {0} -sensing_noise_prob {1}'.format(s,p))
+            cmds.append('-name "sensing_noise experiment_{0}_{1}" -d_x 300 -d_y 100 -sensing_noise_strength {0} -sensing_noise_prob {1}'.format(s,p))
+    return cmds
+    
+def trim_cmd(cmd):    
+    PATTERN = re.compile(r'''((?:[^ "]|"[^"]*")+)''')
+    cmd = PATTERN.split(cmd)[1::2]
+    
+    for i in range(len(cmd)):
+        cmd[i] = re.sub('"','', cmd[i])        
+    return cmd
+
+if __name__ == "__main__":    
+    exp = int(sys.argv[1])
+    N = 5
+    
+    procs = []
+    for _ in range(N):
+        if exp == 1:
+            cmds = compare_swarm_sizes()
+        elif exp == 2:
+            cmds = simple_experiment()
+            #cmds = ['-name "no_maze" -op_xs 500 -op_ys 300 -d_xs 100 -d_ys 300']
+        elif exp == 3:
+            cmds = reliability_experiment()
+        elif exp == 4:
+            cmds = noise_experiment()
+        elif exp == 5:
+            cmds = ordinary_attraction(t=0)
+        elif exp == 6:
+            cmds = moving_disaster()
+        elif exp == 7:
+            cmds = constant_center_repel(ranges = [100])
+        elif exp == 8:
+            cmds = obstacle()
+        elif exp == 9:
+            cmds = maze('simple_new', additions = '-op_xs 500 -op_ys 300 -d_xs 100 -d_ys 300 -cmd_t 5 -cmd "disaster attract"')
+            print(cmds)
+        elif exp == 10:
+            cmds = new_maze('hard_new')
+        elif exp == 11:
+            cmds = maze_experiments(maze_type = 'simple_new')
+        elif exp == 12:
+            cmds = maze_experiments(maze_type = 'hard_new')
+        elif exp == 13:
+            cmds = maze_experiments(maze_type = 'extreme_new')
+        elif exp == 14:
+            cmds = maze_experiments(maze_type = 'simple_new', gp = True)
+        elif exp == 15:
+            cmds = maze_experiments(maze_type = 'hard_new', gp = True)
+        elif exp == 16:
+            cmds = maze_experiments(maze_type = 'extreme_new', gp = True)
+        elif exp == 17:
+            cmds = maze_experiments()
+        elif exp == 18:
+            cmds = maze_experiments(gp = True)            
+        elif exp == 19:
+            cmds = ['-name "distances"']
+        elif exp == 20:
+            cmds = ['-d_size 4 -size 3 -grid_x 100 -grid_y 100 -vis_range 1']
+        elif exp == 21:
+            cmds = communication_noise(communication_noise_prob = [0.1, 0.25, 0.5, 0.75, 1])
+        elif exp == 22:
+            cmds = positioning_noise(positioning_noise_strength = [1, 2, 3], positioning_noise_prob = [0.25, 0.5, 0.75])
+        elif exp == 23:
+            cmds = sensing_noise(sensing_noise_strength = [0.1, 0.25, 0.5, 0.75, 1], sensing_noise_prob = [0.1, 0.25, 0.5, 0.75, 1])
+            
+        for cmd in cmds:            
+            proc = subprocess.Popen([sys.executable, 'init.py'] + trim_cmd(cmd))
+            procs.append(proc)       
+    
+    for proc in procs:
+        proc.wait()        
