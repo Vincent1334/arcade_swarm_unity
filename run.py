3dd3990fb31ce139bafa41788319f853d9487bc1
mdivband <mohammad.divband@gmail.com>
2021-04-25 20:04:43 +0100

    aging changed to 0.995

--- run.py
+++ run.py
@@ -177,20 +177,21 @@ def accuracy_diag(swarm_size = 1, r = 40, alpha = 0.99, t = 300, comm_range = 15
                     + ' -alpha ' + str(alpha) + ' -comm_range ' + str(comm_range)+ ' -run_time ' + str(ex_time))
     return cmds
 
-def user_study1(swarm_size = 15, r = 40, vs_range=2, alpha = 0.99, t = 300, comm_range = 15, exp_type = "user_study", ex_time = 500):
+def user_study1(swarm_size = 15, r = 40, vs_range=2, alpha = 0.99, t = 300, comm_range = 15, exp_type = "user_study", ex_time = 500, aging_factor=0.995):
     cmds = []  
     for exp_num in range(1):
         cmds.append('-name User_Study_1_S{}'.format(swarm_size) + ' -exp_type ' + str(exp_type) + ' -run_time ' + str(ex_time) \
-                    + ' -alpha ' + str(alpha) + ' -comm_range ' + str(comm_range) + ' -size ' + str(swarm_size) + ' -d_move ' + str(True))
+                    + ' -alpha ' + str(alpha) + ' -comm_range ' + str(comm_range) + ' -size ' + str(swarm_size) + ' -d_move ' + str(True) + ' -aging ' + str(aging_factor))
     return cmds
 
-def user_study2(swarm_size = 15,  vs_range=2, vs_radius=40, alpha = 0.99, t = 300, comm_range = 15, exp_type = "user_study_2", ex_time = 500):
+def user_study2(swarm_size = 15,  vs_range=2, vs_radius=40, alpha = 0.99, t = 300, comm_range = 15, exp_type = "user_study_2", ex_time = 500, aging_factor=0.995):
     cmds = []  
     for exp_num in range(1):
         cmds.append('-name User_Study_2_S{}'.format(swarm_size) + ' -exp_type ' + str(exp_type) + ' -run_time ' + str(ex_time) \
                     + ' -alpha ' + str(alpha) + ' -comm_range ' + str(comm_range) + ' -size ' + str(swarm_size) + ' -d_move ' + str(True)
-                    + ' -hum_r ' + str(vs_radius) + ' -vis_range ' + str(vs_range))
+                    + ' -hum_r ' + str(vs_radius) + ' -vis_range ' + str(vs_range) + ' -aging ' + str(aging_factor))
     return cmds
+
 
 def trim_cmd(cmd):    
     PATTERN = re.compile(r'''((?:[^ "]|"[^"]*")+)''')
